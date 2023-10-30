d5f20728d9f200ea69c37237b2f58269a2faba4b
nick-hue <nickagelid@gmail.com>
2023-10-30 18:06:11 +0200

    Initial Commit.

---
+++ Exercise1/dh_assign_1.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <math.h>
+
+void showArgs(char *outputFile, int p, int g, int a, int b);
+void dh_algorithm(char *outputFile, double p, double g, double a, double b);
+
+int main(int argc, char *argv[]) {
+    int opt;
+    char *outputFile = NULL;
+    double p = 0, g = 0, a = 0, b = 0, h = 0;
+
+    while ((opt = getopt(argc, argv, "o:p:g:a:b:h")) != -1) {
+        switch (opt) {
+            case 'o':
+                outputFile = optarg; // getopt sets the optarg variable to point to the argument following -o, the string "output.txt".
+                break;
+            case 'p':
+                p = atoi(optarg);
+                break;
+            case 'g':
+                g = atoi(optarg);
+                break;
+            case 'a':
+                a = atoi(optarg);
+                break;
+            case 'b':
+                b = atoi(optarg);
+                break;
+            case 'h':
+                h = 1;
+                break;
+            default:
+                fprintf(stderr, "Error invalid arguments given.\nUse -h flag to show more info about arguments.\n");
+                exit(EXIT_FAILURE);
+        }
+    }
+
+    if (h == 1){
+        printf("-> [-o Path to output file]\n-> [-p Prime number]\n-> [-g Primitive Root for previous prime number]\n-> [-a Private A key]\n-> [-b Private B key]\n");
+        return 1;
+    }
+        
+    showArgs(outputFile, p, g, a, b);
+    
+    /* if one of the command line flags are not given correctly, exit the program */
+    if (outputFile==NULL || p==0 || g==0 || a==0 || b == 0){
+        fprintf(stderr, "Error invalid arguments given.\nUse -h flag to show more info about arguments.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    /* writes to the output file the <public key A>, <public key B>, <shared secret> computed from the Diffie-Hellman Algorithm */ 
+    dh_algorithm(outputFile, p, g, a, b);
+
+    return 0;
+}
+
+void showArgs(char *outputFile, int p, int g, int a, int b){
+    printf("Output File: %s\n", outputFile);
+    printf("p = %d\n", p);
+    printf("g = %d\n", g);
+    printf("a = %d\n", a);
+    printf("b = %d\n", b);
+}
+
+void dh_algorithm(char *outputFile, double p, double g, double a, double b){
+    double publicA, publicB, secret;
+
+    publicA = fmod(pow(g,a), p);
+    publicB = fmod(pow(g,b), p);
+
+    secret = fmod(pow(publicA, b), p); // Bob's side 
+    // secret = fmod(power(publicB, a), p); // Alice's side
+
+    FILE *f = fopen(outputFile, "w");
+    char fileOutput[40]; // buffer size is 40 because we have a maximum of 10 characters per integer -> 3x10->30 characters and 10 characters for the '<', ',', '>' and '\0'
+    sprintf(fileOutput, "<%d>, <%d>, <%d>", (int)publicA, (int)publicB, (int)secret);
+    fprintf(f, fileOutput);
+    fclose(f);
+}
---
+++ Exercise1/rsa_assign_1.c
@@ -0,0 +1,108 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <math.h>
+
+void showArgs(char *inputFile, char * outputFile, char *keyFile, int keyLength, char * mode);
+void generateRSAKeyPair(int length, int p, int q);
+void writeKeyToFile(char* filename, int key, int keyLength);
+int lambda(int p, int q);
+
+int main(int argc, char *argv[]) {
+    int opt;
+    char *outputFile = NULL, *inputFile = NULL, *keyFile = NULL, *mode = NULL;
+    int keyLength = 0 , h = 0;
+    int sizes[3] = {1024, 2048, 4096};
+
+    while ((opt = getopt(argc, argv, "i:o:k:g:a:deh")) != -1) {
+        switch (opt) {
+            case 'i':
+                inputFile = optarg;
+                break;
+            case 'o':
+                outputFile = optarg;
+                break;
+            case 'k':
+                keyFile = optarg;
+                break;
+            case 'g':
+                keyLength = atoi(optarg);
+                break;
+            case 'd':
+                mode = "decrypt";
+                break;
+            case 'e':
+                mode = "encrypt";
+                break;
+            case 'a':
+                mode = "compare";
+                outputFile = optarg;
+                break;
+            case 'h':
+                h = 1;
+                break;
+            default:
+                fprintf(stderr, "Error invalid arguments given.\nUse -h flag to show more info about arguments.\nHEREERRE");
+                exit(EXIT_FAILURE);
+        }
+    }
+
+    if (h == 1){
+        printf("-> The arguments [-i], [-o] and [-k] are always required when using [-e] or [-d]\n-> Using -i and a path the user specifies the path to the input file.\n-> Using -o and a path the user specifies the path to the output file.\n-> Using -k and a path the user specifies the path to the key file.\n-> Using -g the tool generates a public and a private key given a key length [length] and stores them to the public_length.key and private_length.key files respectively.\n-> Using -d the user specifies that the tool should read the ciphertext from the input file, decrypt it and then store the plaintext in the output file.\n-> Using -e the user specifies that the tool should read the plaintext from the input file, encrypt it and store the ciphertext in the output file.\n-> Using -a the user generates three distinct sets of public and private key pairs, allowing for a comparison of the encryption and decryption times for each.");
+        return 1;
+    }
+        
+    showArgs(inputFile, outputFile, keyFile, keyLength, mode);
+    
+    /* if mode is either encrypt or decrypt and we don't have a input, output, keyFile path produce an error message. */
+    if ((mode=="encrypt" || mode=="decrypt") && (inputFile==NULL || outputFile==NULL || keyFile==NULL)){
+        fprintf(stderr, "Error: while in encrypt/decrypt mode you need the [i],[o],[k] arguments.\nUse -h flag to show more info about arguments.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    if (mode == "compare"){
+        for (int i=0; i<3;i++){
+
+        }
+    }
+
+
+    generateRSAKeyPair(keyLength, 17, 11);
+
+    return 0;
+}
+
+void writeKeyToFile(char* filename, int key, int keyLength){
+    FILE *f = fopen(filename, "w");
+    char fileOutput[keyLength]; 
+    sprintf(fileOutput, "<%d>", key);
+    fprintf(f, fileOutput);
+    printf("Wrote to file: %s\n", filename);
+    fclose(f);
+    printf("Closed file: %s\n", filename);
+}
+
+void generateRSAKeyPair(int length, int p, int q){
+    int n,e; 
+    n = p*q;
+    
+    char *publicPath = "public_length.key";
+    char *privatePath = "private_length.key";
+
+
+
+    writeKeyToFile(publicPath, 13414234, length);
+    writeKeyToFile(privatePath, 5656565, length);
+}
+
+int lambda(int p, int q){
+    return (p-1)*(q-1);
+}
+
+void showArgs(char *inputFile, char * outputFile, char *keyFile, int keyLength, char * mode){
+    printf("Input File: %s\n", inputFile);
+    printf("Output File: %s\n", outputFile);
+    printf("Key File: %s\n", keyFile);
+    printf("Key Length = %d\n", keyLength);
+    printf("Mode = %s\n", mode);
+}
