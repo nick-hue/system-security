e633cce2b8cef28a10f081a9ff2f2f3b459618af
nick-hue <nickagelid@gmail.com>
2023-11-09 17:29:40 +0200

    Trying to implement writing proccess.

--- Exercise2/logger.c
+++ Exercise2/logger.c
@@ -7,14 +7,16 @@
 #include <string.h>
 #include <openssl/evp.h>
 
-size_t getSizeOfFile(FILE *file);
-
-int get_access_type(const char *path, const char *modeString) {
-    if ((strcmp(modeString, "w") == 0) && (access(path, F_OK) != 0)) return 0;      // create
-    if (strcmp(modeString, "r") == 0) return 1;                                     // open
-    if ((strcmp(modeString, "w") == 0) && (access(path, F_OK) == 0)) return 2;      // write
-    return -1;
+__attribute__((constructor))
+void initialize() {
+    // Initialization code here
+    printf("Custom library loaded. Overriding fopen and fwrite.\n");
 }
+
+size_t getSizeOfFile(FILE *file);
+void log_hash_content(FILE *hash_fp, FILE* fout);
+int get_access_type(const char *path, const char *modeString);
+int get_access_denied_flag(const char * path, int access_type);
 
 FILE *fopen(const char* path, const char* mode){
     printf("test function in path: %s\n", path);
@@ -22,8 +24,10 @@ FILE *fopen(const char* path, const char* mode){
     FILE* (*original_fopen)(const char*, const char*);
     original_fopen = dlsym(RTLD_NEXT, "fopen");
 
+    // getting current time 
     time_t t = time(NULL);
-    struct tm tm = *localtime(&t);
+    struct tm tm = *localtime(&t); 
+    
     FILE *fout;
     if (access("file_logging.log", F_OK) == 0){
         fout = (*original_fopen)("file_logging.log", "a");
@@ -36,84 +40,86 @@ FILE *fopen(const char* path, const char* mode){
         exit(1);
     }
 
-    // UID: 1, Filename: "test.txt", Date: 27/11/2001, Timestamp:16:20, Access Type: 0, Is-action-denied flag: 0, File fingerprint: qwoidjq0wijasoijdoijas;
-    printf("UID: %d\n", getuid());
-    printf("Filename: %s\n", path);    
-    printf("Date: %02d/%02d/%d\n", tm.tm_mday, tm.tm_mon + 1, tm.tm_year + 1900);
-    printf("Timestamp: %02d:%02d:%02d\n", tm.tm_hour, tm.tm_min, tm.tm_sec);
     int access_type = get_access_type(path, mode);
-    printf("Access Type: %d\n", access_type);
-
-    int access_flag;
-    if (access_type==0 || access_type== 2)
-    {
-        access_flag = access(path, W_OK);
-    }
-    else if (access_type == 1)
-    {
-        access_flag = access(path, R_OK);
-    }
-    else
-    {
-        access_flag = 1;
-        printf("Error: bad access type\n");
-    }
-    access_flag = -access_flag; // to make the -1 into a 1 if the access function, 0 still is 0; 
-
-    printf("Access denied flag: %d\n", access_flag);
-
-    FILE *fp = (*original_fopen)(path, mode);
-    if (!fp) {
-        printf("failed to open file %s with mode %s\n", path, mode);
-        return NULL;
-    }
+    int access_flag = get_access_denied_flag(path, access_type);
+    
+    fprintf(fout, "UID: %d, Filename: %s, Date: %02d/%02d/%d, Timestamp: %02d:%02d:%02d, Access Type: %d, Access denied flag: %d, File fingerprint: ", getuid(), path, tm.tm_mday, tm.tm_mon + 1, tm.tm_year + 1900, tm.tm_hour, tm.tm_min, tm.tm_sec, access_type, access_flag);
 
     FILE *hash_fp = (*original_fopen)(path, "r");
-    if (hash_fp) {
-        size_t file_size = getSizeOfFile(hash_fp);
-        char *buffer = (char *)malloc(file_size);
-        
-        size_t bytes_read = fread(buffer, 1, file_size, hash_fp);    // reading the contents of the file
-        //printf("File size: %ld\nBytes read: %ld\n", file_size, bytes_read);
-        if (bytes_read != file_size){
-            printf("Error: while reading from file\n");
-            exit(1);
-        }
-        unsigned int hash_length;
-        unsigned char md_value[EVP_MAX_MD_SIZE];    
+    log_hash_content(hash_fp, fout);
 
-        EVP_MD_CTX *mdctx = EVP_MD_CTX_new();
-	    const EVP_MD *EVP_md5();   
-        EVP_DigestInit_ex(mdctx, EVP_md5(), NULL);	    
-        EVP_DigestUpdate(mdctx, buffer, file_size);
-        EVP_DigestFinal_ex(mdctx, md_value, &hash_length);
-        
-        printf("File fingerprint: ");
-        for (size_t i = 0; i < hash_length; i++)    
-        {
-            printf("%02x", md_value[i]);    
-        }
-        printf("\n");
+    fclose(fout);
+    return (*original_fopen)(path,mode);
+}
 
-        // write log to file
-        fprintf(fout, "UID: %d, Filename: %s, Date: %02d/%02d/%d, Timestamp: %02d:%02d:%02d, Access Type: %d, Access denied flag: %d, File fingerprint: ", getuid(), path, tm.tm_mday, tm.tm_mon + 1, tm.tm_year + 1900, tm.tm_hour, tm.tm_min, tm.tm_sec, access_type, access_flag);
-        
-        // write hash to file
-        for (size_t i = 0; i < hash_length; i++)    
-        {
-            fprintf(fout, "%02x", md_value[i]);
-        }
-        fprintf(fout, ";\n");
-        fclose(fout);
-        free(buffer);
-    }
-    else{
+size_t fwrite(const void *ptr, size_t size_of_element, size_t number_of_elements, FILE *stream){
+
+    size_t (*original_fwrite)(const void*, size_t, size_t, FILE *);
+    original_fwrite = dlsym(RTLD_NEXT, "fwrite");
+    
+    printf("Used custom fwrite...\n");
+
+    size_t written = (*original_fwrite)(ptr, size_of_element, number_of_elements, stream);
+
+    return written;
+}
+
+void log_hash_content(FILE *hash_fp, FILE* fout){
+    if (!hash_fp) {
         printf("Error: while trying to get file pointer to read contents from file.\n");
+        
+        fclose(fout);
+        exit(1);
+    }
+    size_t file_size = getSizeOfFile(hash_fp);     // getting the size of the file to hash
+    char *buffer = (char *)malloc(file_size);      // allocating memory 
+    
+    size_t bytes_read = fread(buffer, 1, file_size, hash_fp);    // reading the contents of the file
+    if (bytes_read != file_size){                                // checking if the bytes read are the same with the memory we allocated
+        printf("Error: while reading from file\n");
         exit(1);
     }
 
+    unsigned int hash_length;
+    unsigned char md_value[EVP_MAX_MD_SIZE];    
 
-    return (*original_fopen)(path,mode);
+    EVP_MD_CTX *mdctx = EVP_MD_CTX_new();
+    const EVP_MD *EVP_md5();   
+    EVP_DigestInit_ex(mdctx, EVP_md5(), NULL);	    
+    EVP_DigestUpdate(mdctx, buffer, file_size);
+    EVP_DigestFinal_ex(mdctx, md_value, &hash_length);
+            
+    // write hash to file
+    for (size_t i = 0; i < hash_length; i++)    
+    {
+        fprintf(fout, "%02x", md_value[i]);
+    }
+    fprintf(fout, ";\n");
+
+    fclose(hash_fp);
+    free(buffer);
+}
+
+int get_access_type(const char *path, const char *modeString){
+    if ((strcmp(modeString, "w") == 0) && (access(path, F_OK) != 0)) return 0;      
+    if ((strcmp(modeString, "r") == 0) && (access(path, F_OK) == 0)) return 1;                                     
+    if ((strcmp(modeString, "w") == 0) && (access(path, F_OK) == 0)) return 1;    
+    return -1;
+}
+
+int get_access_denied_flag(const char * path, int access_type){
+    if (access_type==0 || access_type==2)
+    {
+        return -access(path, W_OK);
+    }
+    else if (access_type == 1)
+    {
+        return -access(path, R_OK);
+    }
+    else
+    {
+        return -1;
+    }
 }
 
 size_t getSizeOfFile(FILE *file){ // file has to be already opened 
@@ -122,4 +128,3 @@ size_t getSizeOfFile(FILE *file){ // file has to be already opened
     rewind(file);
     return size;
 }
-    
