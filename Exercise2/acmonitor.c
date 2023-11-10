eccd069b7399dab7012b87dc95ac64486ae7d621
nick-hue <nickagelid@gmail.com>
2023-11-10 17:54:42 +0200

    Started making acmonitor.c

--- Exercise2/acmonitor.c
+++ Exercise2/acmonitor.c
@@ -4,12 +4,14 @@ int main(){
     printf("Calling the fopen() function for WRITING ...\n");
     char str[] = "Hello World13123123132";
 
-    FILE *f = fopen("write.txt", "a");
+    FILE *f = fopen("file_logging.log", "r");
     if (!f){
         printf("Error: opening file\n");
         return 1;
     }
-    fwrite(str, 1, sizeof(str) - 1, f);
+    
+
+
     fclose(f);
 
     printf("Successfully opened file.\n");
