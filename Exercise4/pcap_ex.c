2da08ed48930a17d7e502ddf220e94b160cdbfe4
nick-hue <nickagelid@gmail.com>
2023-11-30 16:48:55 +0200

    first changes branch

--- Exercise4/pcap_ex.c
+++ Exercise4/pcap_ex.c
@@ -36,74 +36,30 @@ int main(int argc, char *argv[]) {
 
     switch(mode){
         case INTERFACE:
-            // online mode i think 
-            pcap_t *handle;			/* Session handle */
-            char *dev;			/* The device to sniff on */
-            char errbuf[PCAP_ERRBUF_SIZE];	/* Error string */
-            struct bpf_program fp;		/* The compiled filter */
-            bpf_u_int32 mask;		/* Our netmask */
-            bpf_u_int32 net;		/* Our IP */
-            struct pcap_pkthdr header;	/* The header that pcap gives us */
-            const unsigned char *packet;		/* The actual packet */
-            pcap_if_t *all_devs;
-
-            if(pcap_findalldevs(&all_devs, errbuf) == -1){
-                fprintf(stderr, "error finding devices");
-                return 1;
-            }
-
-            dev = all_devs->name;
-            printf("Device : %s\n", dev);
-            
-            if (pcap_lookupnet(dev, &net, &mask, errbuf) == -1) {
-                fprintf(stderr, "Can't get netmask for device %s\n", dev);
-                net = 0;
-                mask = 0;
-            }
-
-            handle = pcap_open_live(dev, BUFSIZ, 1, 1000, errbuf);
-            if (handle == NULL) {
-                fprintf(stderr, "Couldn't open device %s: %s\n", dev, errbuf);
-                return(2);
-            }
-
-            if (filter){ // if filter exists apply it, if not dont ¯\_(ツ)_/¯
-                printf("filter exists\nfilter : %s\n", filter);
-
-                if (pcap_compile(handle, &fp, filter, 0, net) == -1) {
-                    fprintf(stderr, "Couldn't parse filter %s: %s\n", filter, pcap_geterr(handle));
-                    return(2);
-                }
-                
-                if (pcap_setfilter(handle, &fp) == -1) {
-                    fprintf(stderr, "Couldn't install filter %s: %s\n", filter, pcap_geterr(handle));
-                    return(2);
-                }
-                //packet = pcap_next(handle, &header);
-
-            } else {
-                printf("filter does not exist\n");
-            }
-
-            printf("Listening...\n");
-
-            pcap_loop(handle, 5, got_packet, NULL);
-            /* Print its length */
-            //printf("Jacked a packet with length of [%d]\n", header.len);
-            /* And close the session */
-            
-            
-            pcap_close(handle);
-
+            // online mode
             break;
         case PACKET:
             // offline mode i think
-            /*
-            char errbuf[PCAP_ERRBUF_SIZE]= {0};
-            pcap_t *test = pcap_open_offline(pcap_name, errbuf);
 
-            printf("buffer : %s\n", errbuf);
-            */
+            pcap_t *handle;			            /* Session handle */
+            char errbuf[PCAP_ERRBUF_SIZE];	    /* Error string */
+            char *dev;                          /* Device Name */
+
+            //dev = argv[2];
+            //printf("Device : %s\n", dev);
+
+            handle = pcap_open_offline(pcap_name, errbuf);
+            if (handle == NULL) {
+                fprintf(stderr, "Couldn't open pcap file %s: %s\n", dev, errbuf);
+                return 2;
+            }
+
+            if (pcap_loop(handle, 0, got_packet, NULL) < 0) {
+                fprintf(stderr, "pcap_loop() failed: %s\n", pcap_geterr(handle));
+                return 3;
+            }
+
+            pcap_close(handle);
             break;
         case HELP:
             printf("-i Select the network interface name (e.g., eth0)\n-r Packet capture file name (e.g., test.pcap)\n-f Filter expression in string format (e.g., port 8080)\n-h Help message, which show the usage of each parameter\n");                
@@ -146,6 +102,6 @@ void got_packet(unsigned char *args, const struct pcap_pkthdr *header, const uns
         return;
     }
     payload = (unsigned char *)(packet + SIZE_ETHERNET + size_ip + size_tcp);
-    printf("payload of the packet : %s\n", payload);
+    //printf("payload of the packet : %s\n", payload);
 
 }
--- Exercise4/pcap_ex.h
+++ Exercise4/pcap_ex.h
@@ -37,25 +37,25 @@ struct sniff_ip {
 #define IP_DF 0x4000		/* don't fragment flag */
 #define IP_MF 0x2000		/* more fragments flag */
 #define IP_OFFMASK 0x1fff	/* mask for fragmenting bits */
-	u_char ip_ttl;		/* time to live */
-	u_char ip_p;		/* protocol */
-	u_short ip_sum;		/* checksum */
+	unsigned char ip_ttl;		/* time to live */
+	unsigned char  ip_p;		/* protocol */
+	unsigned short  ip_sum;		/* checksum */
 	struct in_addr ip_src,ip_dst; /* source and dest address */
 };
 #define IP_HL(ip)		(((ip)->ip_vhl) & 0x0f)
 #define IP_V(ip)		(((ip)->ip_vhl) >> 4)
 
 /* TCP header */
-typedef u_int tcp_seq;
+typedef unsigned int tcp_seq;
 
 struct sniff_tcp {
-	u_short th_sport;	/* source port */
-	u_short th_dport;	/* destination port */
+	unsigned short th_sport;	/* source port */
+	unsigned short th_dport;	/* destination port */
 	tcp_seq th_seq;		/* sequence number */
 	tcp_seq th_ack;		/* acknowledgement number */
-	u_char th_offx2;	/* data offset, rsvd */
+	unsigned char th_offx2;	/* data offset, rsvd */
 #define TH_OFF(th)	(((th)->th_offx2 & 0xf0) >> 4)
-	u_char th_flags;
+	unsigned char th_flags;
 #define TH_FIN 0x01
 #define TH_SYN 0x02
 #define TH_RST 0x04
@@ -65,11 +65,11 @@ struct sniff_tcp {
 #define TH_ECE 0x40
 #define TH_CWR 0x80
 #define TH_FLAGS (TH_FIN|TH_SYN|TH_RST|TH_ACK|TH_URG|TH_ECE|TH_CWR)
-	u_short th_win;		/* window */
-	u_short th_sum;		/* checksum */
-	u_short th_urp;		/* urgent pointer */
+	unsigned short th_win;		/* window */
+	unsigned short th_sum;		/* checksum */
+	unsigned short th_urp;		/* urgent pointer */
 };
 
-void got_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *packet);
+void got_packet(unsigned char *args, const struct pcap_pkthdr *header, const unsigned char *packet);
 
 #endif
